1d96cf59412455a48d091bb909dbe09790482bb0
Jian <coderjian@aliyun.com>
2018-08-29 17:32:37 +0800

    add hot topics in home page.

--- phpBB/includes/functions_display.php
+++ phpBB/includes/functions_display.php
@@ -716,6 +716,226 @@ function display_forums($root_data = '', $display_moderators = true, $return_mod
 	);
 	extract($phpbb_dispatcher->trigger_event('core.display_forums_after', compact($vars)));
 
+
+
+
+
+
+
+
+// Grab all topic data
+	$rowset = $announcement_list = $topic_list = $global_announce_forums = array();
+	$sql = 'SELECT 
+    ht.topicId as topic_id
+FROM
+    pb_hot_topics ht
+WHERE
+    ht.deleted != 1
+ORDER BY ht.lft ASC';
+
+	$result = $db->sql_query_limit($sql, 50, 0);
+
+	while ($row = $db->sql_fetchrow($result))
+	{
+		$topic_list[] = (int) $row['topic_id'];
+	}
+	$db->sql_freeresult($result);
+
+
+
+	$sql = 'SELECT 
+    t.*, f.forum_name
+FROM
+    (pb_topics t)
+        LEFT JOIN
+    pb_forums f ON (f.forum_id = t.forum_id)
+        JOIN
+    pb_hot_topics ht ON t.topic_id = ht.topicId
+        AND ht.deleted != 1
+ORDER BY ht.lft ASC';
+	$result = $db->sql_query($sql, 50, 0);
+
+	while ($row = $db->sql_fetchrow($result))
+	{
+
+		$rowset[$row['topic_id']] = $row;
+		/*$announcement_list[] = $row['topic_id'];
+
+		if ($forum_id != $row['forum_id'])
+		{
+			$global_announce_forums[] = $row['forum_id'];
+		}*/
+	}
+	$db->sql_freeresult($result);
+
+
+	foreach ($topic_list as $topic_id)
+	{
+		$row = &$rowset[$topic_id];
+
+		$topic_forum_id = ($row['forum_id']) ? (int) $row['forum_id'] : $forum_id;
+
+		// This will allow the style designer to output a different header
+		// or even separate the list of announcements from sticky and normal topics
+		$s_type_switch_test = ($row['topic_type'] == POST_ANNOUNCE || $row['topic_type'] == POST_GLOBAL) ? 1 : 0;
+
+		// Replies
+		$replies = $phpbb_content_visibility->get_count('topic_posts', $row, $topic_forum_id) - 1;
+
+		/*if ($row['topic_status'] == ITEM_MOVED)
+		{
+			$topic_id = $row['topic_moved_id'];
+			$unread_topic = false;
+		}
+		else
+		{
+			$unread_topic = (isset($topic_tracking_info[$topic_id]) && $row['topic_last_post_time'] > $topic_tracking_info[$topic_id]) ? true : false;
+		}*/
+		$unread_topic = false;
+
+
+		// Get folder img, topic status/type related information
+		$folder_img = $folder_alt = $topic_type = '';
+//		topic_status($row, $replies, $unread_topic, $folder_img, $folder_alt, $topic_type);
+		topic_status($row, $replies, $unread_topic, $folder_img, $folder_alt, $topic_type);
+
+		// Generate all the URIs ...
+		$view_topic_url_params = 'f=' . $row['forum_id'] . '&amp;t=' . $topic_id;
+		$view_topic_url = $auth->acl_get('f_read', $forum_id) ? append_sid("{$phpbb_root_path}viewtopic.$phpEx", $view_topic_url_params) : false;
+
+		$topic_unapproved = (($row['topic_visibility'] == ITEM_UNAPPROVED || $row['topic_visibility'] == ITEM_REAPPROVE) && $auth->acl_get('m_approve', $row['forum_id']));
+		$posts_unapproved = ($row['topic_visibility'] == ITEM_APPROVED && $row['topic_posts_unapproved'] && $auth->acl_get('m_approve', $row['forum_id']));
+		$topic_deleted = $row['topic_visibility'] == ITEM_DELETED;
+
+		$u_mcp_queue = ($topic_unapproved || $posts_unapproved) ? append_sid("{$phpbb_root_path}mcp.$phpEx", 'i=queue&amp;mode=' . (($topic_unapproved) ? 'approve_details' : 'unapproved_posts') . "&amp;t=$topic_id", true, $user->session_id) : '';
+		$u_mcp_queue = (!$u_mcp_queue && $topic_deleted) ? append_sid("{$phpbb_root_path}mcp.$phpEx", 'i=queue&amp;mode=deleted_topics&amp;t=' . $topic_id, true, $user->session_id) : $u_mcp_queue;
+
+		// Send vars to template
+		$topic_row = array(
+			'FORUM_ID'					=> $row['forum_id'],
+			'TOPIC_ID'					=> $topic_id,
+			'TOPIC_AUTHOR'				=> get_username_string('username', $row['topic_poster'], $row['topic_first_poster_name'], $row['topic_first_poster_colour']),
+			'TOPIC_AUTHOR_COLOUR'		=> get_username_string('colour', $row['topic_poster'], $row['topic_first_poster_name'], $row['topic_first_poster_colour']),
+			'TOPIC_AUTHOR_FULL'			=> get_username_string('full', $row['topic_poster'], $row['topic_first_poster_name'], $row['topic_first_poster_colour']),
+			'FIRST_POST_TIME'			=> $user->format_date($row['topic_time']),
+			'LAST_POST_SUBJECT'			=> censor_text($row['topic_last_post_subject']),
+			'LAST_POST_TIME'			=> $user->format_date($row['topic_last_post_time']),
+			'LAST_VIEW_TIME'			=> $user->format_date($row['topic_last_view_time']),
+			'LAST_POST_AUTHOR'			=> get_username_string('username', $row['topic_last_poster_id'], $row['topic_last_poster_name'], $row['topic_last_poster_colour']),
+			'LAST_POST_AUTHOR_COLOUR'	=> get_username_string('colour', $row['topic_last_poster_id'], $row['topic_last_poster_name'], $row['topic_last_poster_colour']),
+			'LAST_POST_AUTHOR_FULL'		=> get_username_string('full', $row['topic_last_poster_id'], $row['topic_last_poster_name'], $row['topic_last_poster_colour']),
+
+			'REPLIES'			=> $replies,
+			'VIEWS'				=> $row['topic_views'],
+			'TOPIC_TITLE'		=> censor_text($row['topic_title']),
+			'TOPIC_TYPE'		=> $topic_type,
+//			'FORUM_NAME'		=> (isset($row['forum_name'])) ? $row['forum_name'] : $forum_data['forum_name'],
+			'FORUM_NAME'		=> (isset($row['forum_name'])) ? $row['forum_name'] : '主题',
+
+			'TOPIC_IMG_STYLE'		=> $folder_img,
+			'TOPIC_FOLDER_IMG'		=> $user->img($folder_img, $folder_alt),
+			'TOPIC_FOLDER_IMG_ALT'	=> $user->lang[$folder_alt],
+
+			'TOPIC_ICON_IMG'		=> (!empty($icons[$row['icon_id']])) ? $icons[$row['icon_id']]['img'] : '',
+			'TOPIC_ICON_IMG_WIDTH'	=> (!empty($icons[$row['icon_id']])) ? $icons[$row['icon_id']]['width'] : '',
+			'TOPIC_ICON_IMG_HEIGHT'	=> (!empty($icons[$row['icon_id']])) ? $icons[$row['icon_id']]['height'] : '',
+			'ATTACH_ICON_IMG'		=> ($auth->acl_get('u_download') && $auth->acl_get('f_download', $row['forum_id']) && $row['topic_attachment']) ? $user->img('icon_topic_attach', $user->lang['TOTAL_ATTACHMENTS']) : '',
+			'UNAPPROVED_IMG'		=> ($topic_unapproved || $posts_unapproved) ? $user->img('icon_topic_unapproved', ($topic_unapproved) ? 'TOPIC_UNAPPROVED' : 'POSTS_UNAPPROVED') : '',
+
+			'S_TOPIC_TYPE'			=> $row['topic_type'],
+			'S_USER_POSTED'			=> (isset($row['topic_posted']) && $row['topic_posted']) ? true : false,
+			'S_UNREAD_TOPIC'		=> $unread_topic,
+			'S_TOPIC_REPORTED'		=> (!empty($row['topic_reported']) && $auth->acl_get('m_report', $row['forum_id'])) ? true : false,
+			'S_TOPIC_UNAPPROVED'	=> $topic_unapproved,
+			'S_POSTS_UNAPPROVED'	=> $posts_unapproved,
+			'S_TOPIC_DELETED'		=> $topic_deleted,
+			'S_HAS_POLL'			=> ($row['poll_start']) ? true : false,
+			'S_POST_ANNOUNCE'		=> ($row['topic_type'] == POST_ANNOUNCE) ? true : false,
+			'S_POST_GLOBAL'			=> ($row['topic_type'] == POST_GLOBAL) ? true : false,
+			'S_POST_STICKY'			=> ($row['topic_type'] == POST_STICKY) ? true : false,
+			'S_TOPIC_LOCKED'		=> ($row['topic_status'] == ITEM_LOCKED) ? true : false,
+			'S_TOPIC_MOVED'			=> ($row['topic_status'] == ITEM_MOVED) ? true : false,
+
+			'U_NEWEST_POST'			=> $auth->acl_get('f_read', $forum_id) ? append_sid("{$phpbb_root_path}viewtopic.$phpEx", $view_topic_url_params . '&amp;view=unread') . '#unread' : false,
+			'U_LAST_POST'			=> $auth->acl_get('f_read', $forum_id)  ? append_sid("{$phpbb_root_path}viewtopic.$phpEx", $view_topic_url_params . '&amp;p=' . $row['topic_last_post_id']) . '#p' . $row['topic_last_post_id'] : false,
+			'U_LAST_POST_AUTHOR'	=> get_username_string('profile', $row['topic_last_poster_id'], $row['topic_last_poster_name'], $row['topic_last_poster_colour']),
+			'U_TOPIC_AUTHOR'		=> get_username_string('profile', $row['topic_poster'], $row['topic_first_poster_name'], $row['topic_first_poster_colour']),
+			'U_VIEW_TOPIC'			=> $view_topic_url,
+			'U_VIEW_FORUM'			=> append_sid("{$phpbb_root_path}viewforum.$phpEx", 'f=' . $row['forum_id']),
+			'U_VIEW_FORUM_GUIDE'			=> append_sid("{$phpbb_root_path}viewforum.$phpEx", 'f=' . $row['forum_id'].'&amp;g=1'),
+			'U_MCP_REPORT'			=> append_sid("{$phpbb_root_path}mcp.$phpEx", 'i=reports&amp;mode=reports&amp;f=' . $row['forum_id'] . '&amp;t=' . $topic_id, true, $user->session_id),
+			'U_MCP_QUEUE'			=> $u_mcp_queue,
+
+//			'S_TOPIC_TYPE_SWITCH'	=> ($s_type_switch == $s_type_switch_test) ? -1 : $s_type_switch_test,
+			'S_TOPIC_TYPE_SWITCH'	=> -1,
+		);
+
+		/**
+		 * Modify the topic data before it is assigned to the template
+		 *
+		 * @event core.viewforum_modify_topicrow
+		 * @var	array	row					Array with topic data
+		 * @var	array	topic_row			Template array with topic data
+		 * @var	bool	s_type_switch		Flag indicating if the topic type is [global] announcement
+		 * @var	bool	s_type_switch_test	Flag indicating if the test topic type is [global] announcement
+		 * @since 3.1.0-a1
+		 *
+		 * @changed 3.1.10-RC1 Added s_type_switch, s_type_switch_test
+		 */
+		$vars = array('row', 'topic_row', 's_type_switch', 's_type_switch_test');
+		extract($phpbb_dispatcher->trigger_event('core.viewforum_modify_topicrow', compact($vars)));
+
+		$template->assign_block_vars('topicrow', $topic_row);
+
+//		$pagination->generate_template_pagination($view_topic_url, 'topicrow.pagination', 'start', $replies + 1, $config['posts_per_page'], 1, true, true);
+
+		$s_type_switch = ($row['topic_type'] == POST_ANNOUNCE || $row['topic_type'] == POST_GLOBAL) ? 1 : 0;
+
+		/**
+		 * Event after the topic data has been assigned to the template
+		 *
+		 * @event core.viewforum_topic_row_after
+		 * @var	array	row				Array with the topic data
+		 * @var	array	rowset			Array with topics data (in topic_id => topic_data format)
+		 * @var	bool	s_type_switch	Flag indicating if the topic type is [global] announcement
+		 * @var	int		topic_id		The topic ID
+		 * @var	array	topic_list		Array with current viewforum page topic ids
+		 * @var	array	topic_row		Template array with topic data
+		 * @since 3.1.3-RC1
+		 */
+		$vars = array(
+			'row',
+			'rowset',
+			's_type_switch',
+			'topic_id',
+			'topic_list',
+			'topic_row',
+		);
+		extract($phpbb_dispatcher->trigger_event('core.viewforum_topic_row_after', compact($vars)));
+
+		if ($unread_topic)
+		{
+			$mark_forum_read = false;
+		}
+
+		unset($rowset[$topic_id]);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	if ($return_moderators)
 	{
 		return array($active_forum_ary, $forum_moderators);
